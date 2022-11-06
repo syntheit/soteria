98af037aa6322afeeb0df80bdb027c51c1dc9f53
clbertolini <carterbertolini@gmail.com>
2022-11-05 20:04:10 -0400

    Set up file upload to firebase storage

--- pages/new-post.tsx
+++ pages/new-post.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout/Layout";
 import Navbar from "../components/Navbar/Navbar";
+import {ChangeEvent, Dispatch, SetStateAction, useState} from "react";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+const storage = getStorage();
 
 const metadata: { title: string; defaultDescription: boolean } = {
     title: "New Post",
@@ -9,25 +13,50 @@ const metadata: { title: string; defaultDescription: boolean } = {
 
 interface Props {}
 
+
 const NewPost: NextPage<Props> = () => {
+    const [location, setLocation]: [string, Dispatch<SetStateAction<string>>] = useState("");
+    const [description, setDescription]: [string, Dispatch<SetStateAction<string>>] = useState("");
+    const [startTime, setStartTime]: [string, Dispatch<SetStateAction<string>>] = useState("");
+    const [endTime, setEndTime]: [string, Dispatch<SetStateAction<string>>] = useState("");
+    const [images, setImages]: any = useState(null);
+
+    const uploadToClient = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setImages(e.target.files)
+            console.log(e.target.files);
+        }
+    }
+
+    const uploadToServer = () => {
+        if (images && images instanceof FileList) {
+            Array.from(images).forEach(file => uploadBytes(ref(storage, 'images/' + file.name + '.png'), file).then((snapshot) => {
+                console.log('Uploaded a blob or file!');
+            }));
+        }
+    }
+
     return (
         <Layout metadata={metadata}>
             <Navbar currentPage="New Post" />
-            <form className="flex flex-col">
                 <h2>New Post</h2>
 
                 <label htmlFor="location">Location:</label>
-                <input type="text" id="location"/>
+                <input type="text" id="location" onChange={(e) => setLocation(e.target.value)}/>
 
                 <label htmlFor="description">Description:</label>
-                <input type="textarea" id="description"/>
+                <input type="textarea" id="description" onChange={(e) => setDescription(e.target.value)}/>
 
                 <label htmlFor="startTime">Start Time:</label>
-                <input type="time" id="startTime"/>
+                <input type="time" id="startTime" onChange={(e) => setStartTime(e.target.value)}/>
 
-                <label htmlFor="startTime">End Time:</label>
-                <input type="time" id="endTime"/>
-            </form>
+                <label htmlFor="endTime">End Time:</label>
+                <input type="time" id="endTime" onChange={(e) => setEndTime(e.target.value)}/>
+
+                <label htmlFor="images">End Time:</label>
+                <input type="file" id="images" multiple={true} accept="image/png, image/jpeg" onChange={uploadToClient}/>
+
+                <button onClick={uploadToServer}>Post</button>
         </Layout>
     );
 };
