1c6e61a30ee425c12c12e4297148f151328af4b6
syntheit <daniel@matv.io>
2022-11-16 21:43:19 -0500

    Moved sidebar to _app.tsx and did some styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+// use a global state to keep track of the current page
 
 function Soteria({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <Component {...pageProps} />
+    <div className="flex h-[100vh] w-full">
+      <Sidebar />
+      <div className="h-full w-full overflow-y-auto">
+        <Component {...pageProps} />
+      </div>
     </div>
   );
 }
