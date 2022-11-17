32afd0d4218052c417bc71788fb8e3943b174259
syntheit <daniel@matv.io>
2022-11-16 21:42:49 -0500

    Updated links and added icons

--- constants/Links.ts
+++ constants/Links.ts
@@ -1,11 +1,22 @@
-export const links_loggedOut: {
+export const links: {
   label: string;
   url: string;
-  visibleWhenLoggedIn?: boolean;
-}[][] = [
-  [
-    { label: "Home", url: "/", visibleWhenLoggedIn: true },
-    { label: "Our Goal", url: "/goal", visibleWhenLoggedIn: true },
-  ],
-  [{ label: "How To Dispose Properly", url: "/tutorial" }],
+  icon_name?: string;
+  show_only_when_logged_in?: boolean;
+  show_only_when_logged_out?: boolean;
+}[] = [
+  {
+    label: "Home",
+    url: "/",
+    icon_name: "home",
+    show_only_when_logged_out: true,
+  },
+  {
+    label: "Feed",
+    url: "/",
+    icon_name: "inbox",
+    show_only_when_logged_in: true,
+  },
+  { label: "Our Goal", url: "/goal", icon_name: "target-arrow" },
+  { label: "Dispose Properly", url: "/tutorial", icon_name: "recycle" },
 ];
