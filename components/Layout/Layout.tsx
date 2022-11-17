8035742d2f8773a8ab815d1d1006218e4ed1be5f
syntheit <daniel@matv.io>
2022-11-16 21:40:13 -0500

    Switched scss styles to tailwind styles

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,10 @@
-import styles from "./Layout.module.scss";
 import { NextSeo } from "next-seo";
 
 export const defaultDescription = "One man's trash is another man's treasure";
 
 const Layout = ({ children, metadata }) => {
   return (
-    <div className={styles.container}>
+    <div className="bg-black w-full mt-10">
       <NextSeo {...metadata} />
       <main className="flex justify-center">
         <div className="flex justify-center items-center flex-col w-full">
